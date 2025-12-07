1bdf6f0114e223351a59316e3f9b58824059d664
baranov.m <baranov.m@promail366.com>
2025-12-07 19:55:05 +0300

    правка сборки под прод

--- server.prod.js
+++ server.prod.js
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv';
 import path from 'node:path';
-import express from 'express';
 import { readFileSync } from 'node:fs';
-import serialize from 'serialize-javascript';
-import { renderToString } from 'vue/server-renderer';
+import express from 'express';
 import makeApp from './dist-ssr/server/entry-server.js';
-const template = readFileSync('./dist-ssr/client/index.html').toString('utf-8');
+import { renderToString } from 'vue/server-renderer';
+import serialize from "serialize-javascript";
 
 //Определяем прод или тест режим
 const isProd = process.env.NODE_ENV === 'production';
@@ -13,54 +12,55 @@ const environment = isProd ? 'production' : 'dev';
 
 //Подгружаем нужный конфиг .env
 dotenv.config({
-  path: path.resolve(process.cwd(), `.env.${environment}`),
-  quiet: true,
+    path: path.resolve(process.cwd(), `.env.${environment}`),
+    quiet: true,
 });
 
 const port = isProd ? process.env.PORT_PROD : process.env.PORT_DEV;
 
+const template = readFileSync('./dist-ssr/client/index.html').toString('utf-8');
+
 const app = express();
 
+//подключаем ассеты
 app.use('/assets', express.static('dist-ssr/client/assets'));
-// app.use('/img', express.static('dist-ssr/client/img'))
 
 app.use('*all', async (request, response) => {
-  const url = request.originalUrl;
-  const context = { url };
+    const url = request.originalUrl;
+    const context = { url };
 
-  try {
-    let page = template;
-    let status = 200;
-    const { app, router, apiCache } = await makeApp(context);
+    try {
+        let page = template;
+        let status = 200;
+        const { app, router, apiCache } = await makeApp(context);
 
-    const { matched } = router.currentRoute.value;
-    const noSsrPage = matched.some(r => r.meta.guest || r.meta.auth);
+        const { matched } = router.currentRoute.value;
+        const noSsrPage = matched.some(r => r.meta.guest || r.meta.auth);
 
-    if (!noSsrPage) {
-      const html = await renderToString(app);
+        if (!noSsrPage) {
+            const innerHtml = await renderToString(app);
+            page = page.replace(`<!--ssr-outlet-->`, innerHtml);
+        }
 
-      if (status === 200) {
         const serverData = {
-          apiCache,
+            ssr: !noSsrPage,
+            apiCache,
         };
 
-        page = page
-          .replace('<!--ssr-outlet-->', html)
-          .replace(
+        page = page.replace(
             '<!--ssr-data-->',
             `<script>window.appServerData = ${serialize(serverData, { isJSON: true })}</script>`
-          );
-      }
-    }
+        );
 
-    response.status(status).end(page);
-  } catch (error) {
-    console.log(error);
-  }
+        response.status(status).set({ 'Content-Type': 'text/html' }).end(page);
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(port, () => {
-  console.log(
-    `🛠️Режим сборки: ${environment.toUpperCase()} \n🚀 Сервер запущен на http://localhost:${port}`
-  );
+    console.log(
+        `🛠️Режим сборки: ${environment.toUpperCase()} \n🚀 Сервер запущен на http://localhost:${port}`
+    );
 });
